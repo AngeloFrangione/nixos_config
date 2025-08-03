d6a08b4f7e6d17c72787efc1da5e873f18879992
AngeloFrangione <angelo@frangione.fr>
2025-08-03 18:42:02 +0200

    add appimage-run

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
      vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+     appimage-run
      killall
      git
      hfsprogs
